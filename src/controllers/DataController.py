779dbae2ea5d58dcb925b75e15ff4184f0745b67
TahaAhmedT <tahaahmedghoba4y@gmail.com>
2025-11-18 01:14:33 +0200

    rename the generate_unique_filename function to be generate_unique_filepath, and update return values to include file_id

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -19,7 +19,7 @@ class DataController(BaseController):
         
         return True, ResponseSignal.FILE_UPLOAD_SUCCESS.value
     
-    def generate_unique_filename(self, orig_filename: str, project_id: str):
+    def generate_unique_filepath(self, orig_filename: str, project_id: str):
         random_key = self.generate_random_string()
         project_path = ProjectController().get_project_path(project_id=project_id)
 
@@ -37,7 +37,7 @@ class DataController(BaseController):
                 random_key + "_" + cleaned_filename
             )
         
-        return new_file_path
+        return new_file_path, random_key
 
     def get_clean_filename(self, orig_filename: str):
         # remove any special characters, except underscore and .
