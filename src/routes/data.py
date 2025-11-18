b9e370d951cd851984b47f6ec6789fc93c133746
TahaAhmedT <tahaahmedghoba4y@gmail.com>
2025-11-18 02:21:45 +0200

    Add new method that processes endpoinnt to data.py

--- src/routes/data.py
+++ src/routes/data.py
@@ -6,6 +6,8 @@ from controllers import DataController, ProjectController
 import aiofiles
 from models import ResponseSignal
 import logging
+from .schemes.data import ProcessRequest
+from controllers import ProcessController
 
 logger = logging.getLogger("uvicorn.error")
 
@@ -55,3 +57,32 @@ async def upload_data(project_id: str, file: UploadFile,
                 "file_id": file_id
             }
         )
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+
+    process_controller = ProcessController(project_id=project_id)
+
+    file_content = process_controller.get_file_content(file_id=file_id)
+
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+    )
+
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.PROCESSING_FAILED.value
+            }
+        )
+    
+    return file_chunks
+    
