467cd12eff28369f8ecd2b0dc2fb1e5cd2b9e9cb
TahaAhmedT <tahaahmedghoba4y@gmail.com>
2025-11-19 21:35:10 +0200

    Edit calling of id

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -31,7 +31,7 @@ class ProjectModel(BaseDataModel):
     async def create_project(self, project: Project):
 
         result = await self.collection.insert_one(project.dict(by_alias=True, exclude_unset=True)) # insert_one expects dict not object
-        project._id = result.inserted_id
+        project.id = result.inserted_id
 
         return project
     
