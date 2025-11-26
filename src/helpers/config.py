e4e090e9ea249b937c381962c4fb81bb48caf658
TahaAhmedT <tahaahmedghoba4y@gmail.com>
2025-11-26 02:18:31 +0200

    Update config with new parameters

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -29,6 +29,10 @@ class Settings(BaseSettings):
     INPUT_DEFAULT_MAX_TOKENS: int = None
     GENERATION_DEFAULT_TEMPERATURE: float = None
 
+    VECTOR_DB_BACKEND: str
+    VECTOR_DB_PATH: str
+    VECTOR_DB_DISTANCE_METHOD: str = None
+
     class Config:
         env_file = ".env"
 
