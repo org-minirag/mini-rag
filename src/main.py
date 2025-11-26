82176b316ea7435c0ef73e51c4da121203bc34f2
TahaAhmedT <tahaahmedghoba4y@gmail.com>
2025-11-26 21:10:28 +0200

    Update main.py with vector database provider (qdrant) and nlp router

--- src/main.py
+++ src/main.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI
-from routes import base, data
+from routes import base, data, nlp
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
+from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
 
 app = FastAPI()
 
-async def startup_db_client():
+async def startup_spam():
     settings = get_settings()
 
     app.mongo_conn = AsyncIOMotorClient( settings.MONGODB_URL )
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
     llm_provider_factory = LLMProviderFactory(settings)
+    vectordb_provider_factory = VectorDBProviderFactory(settings)
 
     # generation client
     app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
@@ -22,14 +24,21 @@ async def startup_db_client():
     app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
     app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID, 
                                              embedding_size=settings.EMBEDDING_MODEL_SIZE)
+    
+    # vector db client
+    app.vectordb_client = vectordb_provider_factory.create(
+        provider=settings.VECTOR_DB_BACKEND
+    )
+    app.vectordb_client.connect()
 
-async def shutdown_db_client():
+async def shutdown_spam():
     app.mongo_conn.close()
+    app.vectordb_client.disconnect()
 
-app.router.lifespan.on_startup.append(startup_db_client)
-app.router.lifespan.on_shutdown.append(shutdown_db_client)
+app.on_event("startup")(startup_spam)
+app.on_event("shutdown")(shutdown_spam)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
-
+app.include_router(nlp.nlp_router)
 
